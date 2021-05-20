19f5842aec6578f02b604b0c5288c232983c17fd
Tilo Reneau-Cardoso <reneau-cardoso@times-mbp.lan>
2021-05-20 15:56:09 -0700

    first

---
+++ tracker.py
@@ -0,0 +1,128 @@
+import praw
+import json
+import schedule
+import math
+from datetime import datetime
+import concurrent.futures
+
+from reddit import reddit
+
+
+
+class PostTracker:
+    def __init__(self, subreddits):
+        self.latest_post_id = {sub: "" for sub in subreddits}
+        self.posts = {sub: {} for sub in subreddits}
+        self.mature_posts = {sub: {} for sub in subreddits}
+        self.hot = {sub: {} for sub in subreddits}
+        self.subs = [reddit.subreddit(sub) for sub in subreddits]
+        
+    def start_tracking(self):
+        print("Tracking started...")
+
+        def update(sub):
+            print(str(sub) + " starting update...")
+            self.check_for_new_post(sub)
+            self.update_hot(sub)
+            self.update(sub)
+
+        def update_all():
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                executor.map(update,self.subs)
+            self.update_files()
+            self.message()
+                
+        schedule.every().minute.at(":00").do(update_all)
+
+        while True:
+            schedule.run_pending()
+
+    def check_for_new_post (self,sub):
+        try:
+            sub_name = str(sub)
+            new = sub.new(limit=1)
+        except Exception as e:
+            time = self.get_time()
+            print("Error: Check for new post (", time,") ",sub_name)
+            print(e,"\n")
+        else:
+            new_post_id = [submission.id for submission in new][0]
+
+            if self.latest_post_id[sub_name] != new_post_id:
+                self.latest_post_id[sub_name] = new_post_id
+                self.posts[sub_name][self.latest_post_id[sub_name]] = []
+    
+    def update_hot(self,sub):
+        sub_name = str(sub)
+        try:
+            hot = {post.id:post for post in sub.hot(limit = 1000) if not post.stickied }
+        except Exception as e:
+            time = self.get_time()
+            print("Error: Update Hot (", time,")")
+            print(e,"\n")
+        else:
+            self.hot[sub_name] = hot
+    
+    def get_rank(self,post_id, sub):
+        sub_name = str(sub)
+        ar = [post for post in self.hot[sub_name]]
+        try:
+            return ar.index(post_id)+1
+        except:
+            return math.nan
+    
+    def update(self, sub):
+        sub_name = str(sub)
+        time = self.get_time()
+        for post_id in self.posts[sub_name]:
+            rank = self.get_rank(post_id, sub)
+            if math.isnan(rank):
+                self.mature_posts[sub_name][post_id] = self.posts[sub_name].pop(post_id)
+            else:
+                try:
+                    self.posts[sub_name][post_id]
+                except KeyError as e:
+                    print("Error 1 at ",sub_name,": ",e)
+                try:
+                    self.posts[sub_name][post_id].append([time, rank, self.hot[sub_name][post_id].score])
+                except KeyError as e:
+                    print("Error 2 at ",sub_name,": ",e, "(rank = ", rank,")")
+
+    def message(self):
+        message = ""
+        for sub in self.subs:
+            sub_name = str(sub)
+            message += " ||| "+sub_name + " - "
+            message += "\n\tposts: " + str(len(self.posts[sub_name]))
+            message += "\n\tmature posts: " + str(len(self.mature_posts[sub_name]))
+        reddit.redditor('TiloRC').message("UPDATE: "+sub_name, message)
+
+    def update_files(self):
+        file = open("data/data.txt","w")
+        file.write(str(self.posts))
+        file.close()
+
+        file2 = open("data/mature_data.txt","w")
+        file2.write(str(self.mature_posts))
+        file2.close()
+
+    def load_old_data(self):
+        file = open("data/data.txt","r")
+        old_data = file.read().replace("'",'"')
+        self.posts = json.loads(old_data)
+        file.close()
+
+        file2 = open("data/mature_data.txt","r")
+        old_mature_data = file2.read().replace("'",'"')
+        self.mature_posts = json.loads(old_mature_data)
+        file2.close()
+
+    def get_time(self):
+        return datetime.now().strftime("%m:%d:%H:%M:%S")
+
+pt = PostTracker(["pics","WritingPrompts","Lain"])
+pt.start_tracking()
+
+
+     
+
