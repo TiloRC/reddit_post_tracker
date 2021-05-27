b4462993a7b1cb3fb8c46787ed9a9147402cc58c
Tilo Reneau-Cardoso <reneau-cardoso@times-mbp.lan>
2021-05-26 23:00:59 -0700

    lots of refactoring mostly

--- database.py
+++ database.py
@@ -1,56 +1,75 @@
 import sqlite3
 import sys
+import time
 
 conn = sqlite3.connect('data.db')
 
 c = conn.cursor()
 
+default_table = "database"
 
-
-def create_table():
+def create_table(table_name):
 	with conn:
-		c.execute("""CREATE TABLE database (
+		c.execute("CREATE TABLE "+ table_name +""" (
 					subreddit text, post_id text,age real, current_time real, rank int,	score int
 					)""")
 
-def insert(datum):
+def insert(datum, table = default_table):
 	with conn:
-		c.execute("INSERT INTO database VALUES (:subreddit,:post_id,:age,:current_time,:rank,:score)", datum)
+		c.execute("INSERT INTO " +table +" VALUES (:subreddit,:post_id,:age,:current_time,:rank,:score)", datum)
 
 
-def print_subreddit(subreddit):
+def print_subreddit(subreddit, table = default_table):
 	with conn:
-		c.execute("SELECT * FROM database WHERE subreddit = :subreddit", {"subreddit":subreddit})
+		c.execute("SELECT * FROM "+ table+" WHERE subreddit = :subreddit", {"subreddit":subreddit})
 		items = c.fetchall()
 		for item in items:
 			print(item)
 
 
-def print_all():
+def print_all(table = default_table):
 	print("test")
 	with conn:
 		print("huh")
-		c.execute("SELECT * FROM database")
+		c.execute("SELECT * FROM "+table)
 		items = c.fetchall()
 		for item in items:
 			print(item)
 
-
-def clear_all():
+def rename_table(old_name, new_name):
 	with conn:
-		c.execute("DELETE FROM database")
+		c.execute("ALTER TABLE " + old_name + " RENAME TO " +new_name)
+
+def get_size(table = default_table):
+	start = time.time()
+	result = "error"
+	with conn:
+		c.execute("SELECT * FROM " +table)
+		result = len(c.fetchall())
+	end = time.time()
+	if end - start > 1:
+		print("get_size time: ", end-start)
+	return result 
+
+
+def clear_all(table = default_table):
+	with conn:
+		c.execute("DELETE FROM "+table)
 
 try:
-	create_table()
+	create_table(default_table)
 except sqlite3.OperationalError:
 	pass
 
 if __name__ == '__main__':
-		if sys.argv[1] == "print_all":
+		if len(sys.argv) < 2:
+			pass
+		elif sys.argv[1] == "print_all":
 			print_all()
 		elif sys.argv[1] == "print_subreddit":
 			print_subreddit(sys.argv[2])
 		elif sys.argv[1] == "clear_all":
 			clear_all()
+
 
 
--- tracker.py
+++ tracker.py
@@ -13,20 +13,29 @@ import time
 import database
 
 
+
 class PostTracker:
     def __init__(self, subreddits):
-        self.latest_post_id = {sub: "" for sub in subreddits}
-        self.active_posts = {sub: {} for sub in subreddits}
-        self.inactive_posts = {sub: {} for sub in subreddits}
-        self.hot = {sub: {} for sub in subreddits}
-        self.subs = [reddit.subreddit(sub) for sub in subreddits]
+        self.subs = {}
+        for sub in subreddits:
+            self.subs[sub] = {
+                "name": sub,
+                # Holds hot data after it is requested
+                "hot" : {},
+                # Data is stored here during multithreading and then sent to the database
+                "database_queue":[],
+                # An estimate of the number of active posts in each subreddit updated in real time
+                "num_active_posts": 300,
+                # Stores the post id of the newest post in each subreddit
+                "latest_post_id" : "",
+                # Stores the current hot rank of the newest post id for each subreddit. Used to estimate num_active_posts.
+                "latest_post_rank": []
+            }
+
         self.database_queue = []
-        self.num_active_posts = {sub: 300 for sub in subreddits}
-        self.latest_post_rank = {sub: [] for sub in subreddits}
         
     def start_tracking(self):
         print("Tracking started...")
-
         def update(sub):
             print(str(sub) + " starting update...")
             self.check_for_new_post(sub)
@@ -40,10 +49,15 @@ class PostTracker:
                 with concurrent.futures.ThreadPoolExecutor() as executor:
                     executor.map(update,self.subs)
             else:
-                update(self.subs[0])
-            self.update_database()
+                update([sub for sub in self.subs][0])
+            for item in self.database_queue:
+                #print(item)
+                database.insert(item,"database")
+            self.database_queue = []
+                
             self.message()
             end = time.time()
+            print("number of datapoints: ",database.get_size())
             print("Update Time: ",end-start, " sec")
                 
         schedule.every().minute.at(":00").do(update_all)
@@ -52,91 +66,75 @@ class PostTracker:
 
     def check_for_new_post (self,sub):
         try:
-            sub_name = str(sub)
-            new = sub.new(limit=1)
+            new = reddit.subreddit(sub).new(limit=1)
         except Exception as e:
-            time = self.get_time()
-            print("Error: Check for new post (", time,") ",sub_name)
-            print(e,"\n")
+            #time = self.get_time()
+            #print("Error: Check for new post (", time,") ",sub_name)
+            #print(e,"\n")
+            pass
         else:
-            new_post_id = [submission.id for submission in new][0]
-
-            if self.latest_post_id[sub_name] != new_post_id:
-                self.latest_post_id[sub_name] = new_post_id
-                self.active_posts[sub_name][self.latest_post_id[sub_name]] = []
+            self.subs[sub]["latest_post_id"] = [submission.id for submission in new][0]
     
     def update_hot(self,sub):
-        sub_name = str(sub)
-        limit = self.num_active_posts[sub_name]
+        limit = self.subs[sub]["num_active_posts"]
         try:
-            hot = {post.id:post for post in sub.hot(limit = limit) if not post.stickied }
+            hot = {post.id:post for post in reddit.subreddit(sub).hot(limit = limit) if not post.stickied }
         except Exception as e:
             time = self.get_time()
             print("Error: Update Hot (", time,")")
             print(e,"\n")
         else:
-            self.hot[sub_name] = hot
+            self.subs[sub]["hot"] = hot
     
     def get_rank(self,post_id, sub):
-        sub_name = str(sub)
-        ar = [post for post in self.hot[sub_name]]
+        ar = [post for post in self.subs[sub]["hot"]]
         try:
             return ar.index(post_id)+1
         except:
             return math.nan
 
     def get_post_by_rank(self,rank,sub):
-        sub_name = str(sub)
-        ar = [post for post in self.hot[sub_name]]
+        ar = [post for post in self.subs[sub]["hot"]]
         return ar[rank-1]
     
     def update(self, sub):
-        sub_name = str(sub)
-        for post_id in self.active_posts[sub_name].copy():
+        for post_id in self.subs[sub]["hot"]:
             rank = self.get_rank(post_id, sub)
-            if math.isnan(rank):
-                self.inactive_posts[sub_name][post_id] = self.active_posts[sub_name].pop(post_id)
-            else:
-                post = self.hot[sub_name][post_id]
-                #self.active_posts[sub_name][post_id].append([date_time, rank, post.score]) 
-                self.database_queue.append({"subreddit":sub_name,
-                                            "post_id":post_id,
-                                            "age":time.time()-post.created_utc,
-                                            "current_time":time.time(), 
-                                            "rank":rank,
-                                            "score": post.score})
+            post = self.subs[sub]["hot"][post_id]
+            self.database_queue.append({"subreddit":sub,
+                                        "post_id":post_id,
+                                        "age":time.time()-post.created_utc,
+                                        "current_time":time.time(), 
+                                        "rank":rank,
+                                        "score": post.score})
+
+
 
     def message(self):
         message = ""
         for sub in self.subs:
-            sub_name = str(sub)
-            message += " ||| "+sub_name + " - "
-            message += "active posts: " + str(len(self.active_posts[sub_name]))
-            message += " inactive posts: " + str(len(self.inactive_posts[sub_name]))
+            message += " ||| "+sub + " - "
+            message += "estimated active posts: " + str(self.subs[sub]["num_active_posts"])
         print(message)
         #reddit.redditor("myUsername").message("UPDATE", message)
 
-    def update_database(self):
-        for item in self.database_queue:
-            database.insert(item)
-        self.database_queue = []
-
     def update_active_posts(self, sub):
-        sub = str(sub)
-        post_id = self.latest_post_id[sub]
-        self.latest_post_rank[sub].append(self.get_rank(post_id,sub))
-        num_active_posts = 10 + 2*int(stats.mean(self.latest_post_rank[sub]))
+        post_id = self.subs[sub]["latest_post_id"]
+        self.subs[sub]["latest_post_rank"].append(self.get_rank(post_id,sub))
+        rank_history = [rank for rank in self.subs[sub]["latest_post_rank"] if not math.isnan(rank)]
+        if len(rank_history) > 0:
+            num_active_posts = 10 + 2*int(stats.mean(rank_history))
 
-        if math.isnan(num_active_posts):
-            self.num_active_posts[sub] += 100
-            print("nan. ",self.num_active_posts[sub])
-        else:
-            self.num_active_posts[sub] = num_active_posts 
-            #print(sub," # active_posts: ",num_active_posts,end =" - ")
-            #try:
-            #	print(sub+"/comments/"+str(self.get_post_by_rank(num_active_posts,sub)))
-            #except IndexError:
-            #	print("index error")
+            if math.isnan(num_active_posts) and self.subs[sub]["num_active_posts"] < 1000:
+                self.subs[sub]["num_active_posts"] += 100
+                #print("nan. ",self.num_active_posts[sub])
+            else:
+                self.subs[sub]["num_active_posts"] = num_active_posts 
+                #print(sub," # active_posts: ",num_active_posts,end =" - ")
+                #try:
+                #	print(sub+"/comments/"+str(self.get_post_by_rank(num_active_posts,sub)))
+                #except IndexError:
+                #	print("index error")
 
 
 
